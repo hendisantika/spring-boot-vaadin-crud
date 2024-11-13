e10a1fdde1e8ad3a366277cb1101e4f35a8cfb34
hendisantika <hendisantika@yahoo.co.id>
2024-11-13 17:53:34 +0700

    Init Data🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vaadincrud/SpringBootVaadinCrudApplication.java
+++ src/main/java/id/my/hendisantika/vaadincrud/SpringBootVaadinCrudApplication.java
@@ -1,7 +1,11 @@
 package id.my.hendisantika.vaadincrud;
 
+import id.my.hendisantika.vaadincrud.model.Person;
+import id.my.hendisantika.vaadincrud.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootVaadinCrudApplication {
@@ -10,4 +14,14 @@ public class SpringBootVaadinCrudApplication {
         SpringApplication.run(SpringBootVaadinCrudApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner loadData(PersonRepository repository) {
+        return (args) -> {
+            // Add some data to database
+            repository.save(new Person("Itadori", "Yuji"));
+            repository.save(new Person("Megumi", "Fushiguro"));
+            repository.save(new Person("Gojo", "Satoru"));
+            repository.save(new Person("Ryoumen", "Sukuna"));
+        };
+    }
 }
